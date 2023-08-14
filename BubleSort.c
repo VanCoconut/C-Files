ef7cd65889aa9225517c307199c0ca418e053e24
Vincenzo Catalano <ing.v.catalano@gmail.com>
2023-08-14 18:18:13 +0200

    sort char

--- BubleSort.c
+++ BubleSort.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
-void sort(int array[], int size);
+void sortInt(int array[], int size);
+void sortChar(char array[], int size);
 void displayIntArray(int array[], int size);
+void displayCharArray(char array[], int size);
 
 int main()
 {
-    int array[] = {2, 5, 3, 9, 6, 4, 1};
-    int size = sizeof(array) / sizeof(array[0]);
-    displayIntArray(array, size);
-    printf("-----------------------------\n");
-    sort(array, size);
-    displayIntArray(array, size);
+    int numbers[] = {2, 5, 3, 9, 6, 4, 1};
+    char letter[] = {'C', 'E', 'B', 'A'};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    // INT
+    // displayIntArray(numebrs, size);
+    // sort(numbers, size);
+    // displayIntArray(numbers, size);
+
+    // CHAR
+    displayCharArray(letter, size);
+    sortChar(letter, size);
+    displayCharArray(letter, size);
 
     return 0;
 }
 
-void sort(int array[], int size)
+void sortInt(int array[], int size)
 {
     for (size_t i = 0; i < size - 1; i++)
     {
@@ -30,11 +39,48 @@ void sort(int array[], int size)
         }
     }
 }
-
+void sortChar(char array[], int size)
+{
+    for (size_t i = 0; i < size - 1; i++)
+    {
+        for (size_t j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                char temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+/* vai a capire perchè funziona e nel funzionare ordine in senso opposto al segno
+void sort(int array[], int size)
+{
+    for (size_t i = 0; i < size ; i++)
+    {
+        for (size_t j = 0; j < size ; j++)
+        {
+            if (array[i] > array[j])
+            {
+                int temp = array[j];
+                array[j] = array[i];
+                array[i ] = temp;
+            }
+        }
+    }
+}*/
 void displayIntArray(int array[], int size)
 {
     for (size_t i = 0; i < size; i++)
     {
         printf("%d\t", array[i]);
+    }
+}
+void displayCharArray(char array[], int size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%c\t", array[i]);
     }
 }
