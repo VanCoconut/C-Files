5e05819022b4016d23af318a68234c3b4d5a2304
Vincenzo Catalano <ing.v.catalano@gmail.com>
2023-08-15 18:27:55 +0200

    write and delete files

---
+++ WriteFiles.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+   // WRITE/APPEND A FILE "w" to overwrite and "a" append use only absolutre path
+   FILE *pF = fopen("C:\\Users\\Vincenzo Catalano\\Git\\C-Files\\test.txt", "w");
+
+   fprintf(pF, "Spongebob Squarepants");
+
+   fclose(pF);
+   
+   // DELETE A FILE
+   
+   if(remove("C:\\Users\\Vincenzo Catalano\\Git\\C-Files\\test.txt") == 0)
+   {
+      printf("That file was deleted successfully!");
+   }
+   else
+   {
+      printf("That file was NOT deleted!");
+   }
+   
+   return 0;
+}
