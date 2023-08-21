dd9762a063f34e32d8ca0728f4cc5b1721ef94b7
Vincenzo Catalano <ing.v.catalano@gmail.com>
2023-08-21 12:40:23 +0200

    what happens without pointers

--- BubleSort.c
+++ BubleSort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
+int descending(int, int);
 int ascending(int, int);
-void swap(int *, int *);
 void bubleSort(int[], int, int (*)(int, int));
 
 int main()
@@ -11,47 +11,58 @@ int main()
     bubleSort(a, size, ascending);
     for (size_t i = 0; i < size; i++)
     {
-        printf("%5d",a[i]);
+        printf("%5d", a[i]);
     }
-    
+
     return 0;
 }
 
 void bubleSort(int a[], int size, int (*compare)(int x, int y))
 {
-    int descending(int, int);
-    int ascending(int, int);
-    void swap(int *, int *);
+    void swap(int, int);
+    void swapP(int *, int *);
 
     for (size_t i = 0; i < size - 1; i++)
     {
         for (size_t j = 0; j < size - i - 1; j++)
         {
-           // int c=(*compare)(a[j], a[j + 1]);
-           // printf("\n%d\n",c);
+            // int c=(*compare)(a[j], a[j + 1]);
+            // printf("\n%d\n",c);
             if ((*compare)(a[j], a[j + 1]))
             {
-                swap(&a[j], &a[j + 1]);
+                swapP(&a[j], &a[j + 1]);
+            //printf("a[j] = %d, a[j+1] = %d\n",a[j],a[j+1]);
             }
         }
     }
 }
 
-//non modifica valori quindi non ha bisogno di puntatori
+// non modifica valori quindi non ha bisogno di puntatori
 int ascending(int a, int b)
 {
     // c doesn't have boolean so 0 is "false" 1 "true"
-    return b < a; 
+    return b < a;
 }
 int descending(int a, int b)
 {
     // c doesn't have boolean so 0 is "false" 1 "true"
-    return b > a; 
+    return b > a;
 }
 
-void swap(int *a, int *b)
+//se non si usano i puntatori la funzione cambia i valori solo al suo interno e non nel ciclo for di buble
+void swapP(int *a, int *b)
 {
     int hold = *a;
     *a = *b;
     *b = hold;
+    //printf("a = %d, b = %d\n",*a,*b);
+}
+
+//se non si usano i puntatori la funzione cambia i valori solo al suo interno e non nel ciclo for di buble
+void swap(int a, int b)
+{
+    int hold = a;
+    a = b;
+    b = hold;
+    printf("a = %d, b = %d\n",a,b);
 }
