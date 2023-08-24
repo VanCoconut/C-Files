efeff894a8d3069c541a53fa81fd2eafa060d0cf
Vincenzo Catalano <ing.v.catalano@gmail.com>
2023-08-24 14:33:03 +0200

    trimming scanner

--- Scanner.c
+++ Scanner.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
- 
-/*
-    scanf not allow white space therefore it will affect following scans
+int main()
+{
 
-    fgets avoid this inconvinient but remeber that it will ad \n ad the end of the scanned line
+  /*
+      scanf not allow white space therefore it will affect following scans
 
-*/
-    
-    int age;
-    char name[25];
+      fgets avoid this inconvinient but remeber that it will ad \n ad the end of the scanned line
+
+  */
+
+  int age;
+  int x, y;
+  char name[25];
+  const char *cosa;
+  const char *scarto;
+  //senza lo scarto non si possono immagazzinare 2 stringhe diverse se si inserisce una prima strigna più lunga di 2
+  scanf("%2s%s%2s",name,scarto,cosa); 
+  printf("nome = %s ,scarto = %s cosa = %s ",name,scarto,cosa);
+  //scanf("%d %d",&x,&y);
+  //printf("x = %d y = %d", x, y);
   //  printf("How old are you?\n");
-    printf("What's your name?\n");
+  // printf("What's your name?\n");
   //  scanf("%d",&age);
-   // scanf("%s",&name);
-   fgets(name,25,stdin);
-   name[strlen(name)-1]= '\0';
-   // printf("You are %d years old",age);
-    printf("Hello %s ",name);
-    return 0;
+  // scanf("%s",&name);
+  // fgets(name,25,stdin);
+  // name[strlen(name)-1]= '\0';
+  // printf("You are %d years old",age);
+  // printf("Hello %s ",name);
+  return 0;
 }
