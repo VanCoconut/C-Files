78127c2a63a73c9bfe4b896da9ef20087a284c6d
vcata <ing.v.catalano@gmail.com>
2024-03-29 16:47:24 +0100

    revert modify on ArrayCopy.c

--- Array/ArrayCopy.c
+++ Array/ArrayCopy.c
@@ -3,11 +3,19 @@
 
 int main()
 {
-    //int a1[] = {1, 2, 3};
-     int a1 = 7;
-    int *a2=&a1;
-    *a2=3;
+    int a1[] = {1, 2, 3};
+    int *a2 = a1;
+    int a3[3];
+    a2[0] = 0;
 
-     printf("%d\t", *a2);
+    memcpy(a3, a1, sizeof(a1));
+
+    a3[1]=5;
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        printf("%d\t", a1[i]);
+    }
+
     return 0;
 }
